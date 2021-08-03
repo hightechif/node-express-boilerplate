63b5e2a775d82ff7b7f63bafaa552a6a28e7057a
R. Fadhil <ridhanfadhilah@gmail.com>
2021-08-03 14:37:13 +0700

    express app

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 // Import Module
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
 // Import router
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api.routes');
+const usersRouter = require('./routes/users');
 
 // Import Middleware
 const errorMiddleware = require('./utils/error.middleware');
 
 // Activate Express App
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Middlewares
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Router
 app.use('/', indexRouter);
+app.use('/api/v1', apiRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("/api/v1")
-});
-
-router.get('/view', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ title: 'Express API Running' });
 });
 
 module.exports = router;
